6f7c958b1a6eaad526f64a35d9d36082eb753fd0
VinuelMancent <61296588+VinuelMancent@users.noreply.github.com>
2022-04-21 09:11:32 +0200

    added javaDoc comments

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -2,16 +2,28 @@ package de.hfu;
 
 import java.util.Scanner;
 /**
- * Hello world!
+ * Hauptklasse App
  *
  */
 public class App 
 {
+	/**
+	* Main Methode
+	*/
     public static void main( String[] args )
     {
+    	/**
+    	* Scanner Keyboard wird initialisiert
+    	*/
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please write whatever you want");
+        /**
+        * Gets user input
+        */
         String userInput = keyboard.next();
+        /**
+        * Output user input
+        */
         System.out.println("Your input in only uppercase: " + userInput.toUpperCase());
     }
 }
