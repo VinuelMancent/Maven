153ac1d1b11681511e31842182aa113575f8c7ef
VinuelMancent <61296588+VinuelMancent@users.noreply.github.com>
2022-04-28 13:35:42 +0200

    added a test for dequeue

--- src/test/java/de/hfu/QueueTest.java
+++ src/test/java/de/hfu/QueueTest.java
@@ -26,15 +26,24 @@ public class QueueTest
     	queue.enqueue(1);
     	queue.enqueue(2);
     	queue.enqueue(3);
-    	assertEquals("should be 1",queue.dequeue(),1);
     	queue.enqueue(4);
     	queue.enqueue(5);
     	queue.enqueue(6);
-    	queue.enqueue(7);
-    	assertEquals("should be 2",queue.dequeue(), 2);    	
-    	assertEquals("should be 3",queue.dequeue(), 3);
-    	assertEquals("should be 4",queue.dequeue(), 7);
+    	//queue.enqueue(7);
+    	assertEquals("should be 1",queue.queue[0], 1);    	
+    	assertEquals("should be 2",queue.queue[1], 2);
+    	assertEquals("should be 6",queue.queue[2], 6);
     	
+    }
+    
+    @Test
+    public void testDequeue(){
+    	queue.enqueue(1);
+    	queue.enqueue(2);
+    	queue.enqueue(3);
+    	assertEquals("should be 1",queue.dequeue(),1);
+    	assertEquals("should be 2",queue.dequeue(),2);
+    	assertEquals("should be 3",queue.dequeue(),3);
     }
     
     @Test(expected=IllegalStateException.class)
